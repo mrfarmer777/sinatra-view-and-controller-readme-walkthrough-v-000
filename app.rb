e7218fd615a3fd2c2bd45a0c0790653c3146572b
Matt Farmer <mrfarmer777@gmail.com>
2018-06-28 01:34:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,16 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    erb :reversed
+    ostring=params["string"]  #take the string passed to the route
+    @reversed_string=ostring.reverse #the instance variable makes it possible to pass this data to any view or route
+
+    erb :reversed   #now reversed can get the reversed string variable because it's an instance variable, not between controllers though
   end
 
   get '/friends' do
     # Write your code here!
+    @friends=['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
 end
